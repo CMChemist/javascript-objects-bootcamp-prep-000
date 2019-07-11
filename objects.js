0abe41f51ed8df1f08dd4cd9a789539cd9288140
global-value-0259 <josephtrettevik@gmail.com>
2019-07-11 19:37:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,6 @@
 var playlist = new Object ({Beyonce: "Formation"});
-var i = 0;
 
 function updatePlaylist (obj , artistName, songTitle) {
-  /*if (i === 0) {
-    removeFromPlaylist (obj, 'Beyonce');
-    i++;
-  }*/
   return Object.assign(obj, { [artistName]: songTitle});
 }
 
